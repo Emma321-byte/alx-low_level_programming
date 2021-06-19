e59a83d6ef0685fd7ae25c5b0487249288f763ab
Emma321-byte <obrim89#gmail.com>
2021-06-19 08:41:39 +0200

    print 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *main-use print
+ *Return: print ten numbers from zero
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int count;
+
+	/* your code goes there */
+	for (count = 0; count < 10; count++)
+	{
+	printf("%d", count);
+	}
+	printf("\n");
+	return (0);
+}
+
