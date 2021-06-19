a4b139223f31d572bb712c6e6452f2284fb04c83
Emma321-byte <obrim89#gmail.com>
2021-06-19 06:42:40 +0200

    print lower_case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *main-use putchar
+ *Return: small case alphabets
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char small_alphabet;
+
+	small_alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	/* your code goes there */
+	putchar(small_alphabet)
+	return (0);
+}
+
+
+
