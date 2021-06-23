ba49d5a0c70446732896cce4b8c41bff5948d0d6
Emma321-byte <obrim89#gmail.com>
2021-06-23 18:08:22 +0200

    small alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/* more headers goes there */
+
+/**
+ *main - prototype in holberton.h
+ *
+ *Return: fuction for lowercase charactors
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
+	void print_alphabet()
+	{
+	int i;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i<26; i++)
+	{
+	putchar(alpha[i]);
+	}
+	}
